2919e2a2dd365e4a835c6d94bd3b1b286c567945
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-08-15 12:15:55 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import Form from './Form'; // Assuming the Form component is in a file named Form.js
+
+const fields = [
+  { name: 'firstName', type: 'text', label: 'Name' },
+  { name: 'email', type: 'email', label: 'Email' },
+  { name: 'password', type: 'password', label: 'Password' },
+  
+];
 
 const App = () => {
+  const handleSubmit = (values) => {
+    console.log('Form submitted with values:', values);
+    // You can perform further actions with the submitted values here
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Sample Form</h1>
+      <Form onSubmit={handleSubmit} fields={fields} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
